0a55ae9521f33ad4ec2d32b657a8b9301c1f4516
EremizinA <sivanochko@deloitte.com>
2023-10-16 11:40:16 +0300

    changes

---
+++ src/WeatherForecast.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WeatherForecast.Api.Controllers;
+using WeatherForecast.Api.Models;
+
+namespace WeatherForecast.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WeatherTodayController _weatherTodayController;
+        private readonly ILogger<IndexModel> _logger;
+        
+        [BindProperty]
+        public string ErrorMessage { get; set; }
+
+        [BindProperty]
+        public List<WeatherToday> WeatherTodayList { get; set; }
+
+        public IndexModel(WeatherTodayController weatherTodayController, ILogger<IndexModel> logger)
+        {
+            _weatherTodayController = weatherTodayController;
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            var result = await _weatherTodayController.GetAll();
+            var okResult = result as OkObjectResult;
+            if (okResult != null)
+            {
+                var weatherList = okResult.Value as List<WeatherToday>;
+                if(weatherList != null)
+                {
+                    WeatherTodayList = weatherList;
+                }
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            return Page();
+        }
+    }
+}
---
+++ src/WeatherForecast.UI/Program.cs
@@ -0,0 +1,50 @@
+using System.Reflection;
+using WeatherForecast.Api.Controllers;
+using WeatherForecast.Api.Data;
+using WeatherForecast.Api.Logging;
+using WeatherForecast.Api.Options;
+using WeatherForecast.Api.Repositories;
+using WeatherForecast.Api.Services;
+
+var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddUserSecrets(Assembly.GetExecutingAssembly());
+
+var config = configBuilder.Build();
+
+var host = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+host.Services.AddRazorPages();
+
+host.Services.Configure<DbConnectionOptions>(config.GetSection(nameof(DbConnectionOptions)));
+
+host.Services.AddSingleton<ISqlServerDbConnectionFactory, SqlServerDbConnectionFactory>();
+
+host.Services.AddSingleton<IWeatherTodayRepository, WeatherTodayRepository>();
+host.Services.AddSingleton<IWeatherTodayService, WeatherTodayService>();
+host.Services.AddTransient(typeof(ILoggerAdapter<>), typeof(LoggerAdapter<>));
+host.Services.AddSingleton<WeatherTodayController, WeatherTodayController>();
+
+var app = host.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
