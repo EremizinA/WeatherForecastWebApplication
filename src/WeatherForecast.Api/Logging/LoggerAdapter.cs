bc9f78538e4b0b97e9bb0a0615807be33099b637
EremizinA <sivanochko@deloitte.com>
2023-10-17 08:39:54 +0300

    add logging timestamp

--- src/WeatherForecast.Api/Logging/LoggerAdapter.cs
+++ src/WeatherForecast.Api/Logging/LoggerAdapter.cs
@@ -1,8 +1,12 @@
-﻿namespace WeatherForecast.Api.Logging;
+﻿using System;
+
+namespace WeatherForecast.Api.Logging;
 
 public class LoggerAdapter<TType> : ILoggerAdapter<TType>
 {
     private readonly ILogger<TType> _logger;
+
+    private static string DateTimeUtcNow => $"[{DateTime.UtcNow:yyyy/MM/dd HH:mm:ss}] ";
 
     public LoggerAdapter(ILogger<TType> logger)
     {
@@ -11,11 +15,11 @@ public class LoggerAdapter<TType> : ILoggerAdapter<TType>
 
     public void LogInformation(string? message, params object?[] args)
     {
-        _logger.LogInformation(message, args);
+        _logger.LogInformation(DateTimeUtcNow + message, args);
     }
 
     public void LogError(Exception exception, string message, params object?[] args)
     {
-        _logger.LogError(exception, message, args);
+        _logger.LogError(exception, DateTimeUtcNow + message, args);
     }
 }
